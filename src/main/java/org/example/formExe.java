46322dbbc97ac7d032a8368ad3862c7f8d1de82a
Damazy <damazy5@wp.pl>
2023-06-29 10:15:13 +0200

    trying to add some swing gui

--- src/main/java/org/example/formExe.java
+++ src/main/java/org/example/formExe.java
@@ -1,52 +1,93 @@
 package org.example;
 
+import java.awt.*;
+import java.awt.image.ImageProducer;
 import java.io.*;
 import javax.swing.*;
 import java.awt.event.*;
 import javax.swing.filechooser.*;
 
-public class formExe extends JFrame implements ActionListener {
+public class formExe extends JPanel implements ActionListener {
 
-    static JLabel l;
+    static private final String newline = "\n";
+    JButton openButton;
+    JTextArea log;
+    JFileChooser fc;
 
-    formExe() {
-    }
+    public formExe() {
+        super(new BorderLayout());
 
-    ;
+        log = new JTextArea(5, 20);
+        log.setMargin(new Insets(5, 5, 5, 5));
+        log.setEditable(false);
+        JScrollPane logScrollPane = new JScrollPane(log);
 
-    public static void main(String[] args) {
-        JFrame f = new JFrame("Microsoft Documents Reader");
-        f.setLayout(null);
-        f.setSize(400, 400);
-        f.setVisible(true);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JButton button = new JButton("Otwórz");
-        button.setLayout(null);
-        formExe fe = new formExe();
-        button.addActionListener(fe);
-        JPanel p = new JPanel();
-        p.setLayout(null);
-        button.setLocation(20,20);
-        p.add(button);
-        button.setVisible(true);
+        fc = new JFileChooser();
 
-        l = new JLabel("wybierz plik xlsx");
-        l.setHorizontalTextPosition(SwingConstants.LEFT);
 
-        p.add(l);
-        f.add(p);
-        f.show();
+        Icon icon = new ImageIcon("Open16.gif");
+        openButton = new JButton("Open a file...",icon);
+        openButton.addActionListener(this);
 
+        JPanel buttonPanel = new JPanel(); //use FlowLayout
+        buttonPanel.add(openButton);
+
+        add(buttonPanel, BorderLayout.PAGE_START);
+        add(logScrollPane, BorderLayout.CENTER);
 
     }
+
+
+
+
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        String com = e.getActionCommand();
-        if (com.equals("Otwórz")) {
-            JFileChooser j = new JFileChooser();
-            j.showSaveDialog(null);
+        int returnVal = fc.showOpenDialog(formExe.this);
+
+        if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+            log.append("Opening: " + file.getName() + "." + newline);
+        } else {
+            log.append("Open command cancelled by user." + newline);
         }
+        log.setCaretPosition(log.getDocument().getLength());
 
     }
+
+    protected static ImageIcon createImageIcon(String path) {
+        java.net.URL imgURL = formExe.class.getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("MicrosoftDocumentReader");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,400);
+
+        //Add content to the window.
+        frame.add(new formExe());
+
+        //Display the window.
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                //Turn off metal's use of bold fonts
+                UIManager.put("swing.boldMetal", Boolean.FALSE);
+                createAndShowGUI();
+            }
+        });
+
+    }
+
+
+
 }
